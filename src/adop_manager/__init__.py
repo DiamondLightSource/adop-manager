4073b9330047d4d0c83c1c2387d9686cb1c48e02
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-07-15 15:07:45 +0100

    Added AdOpManager to __init__.py

--- src/adop_manager/__init__.py
+++ src/adop_manager/__init__.py
@@ -1,5 +1,6 @@
 from ._version_git import __version__
+from .app import AdOpManager
 
 # __all__ defines the public API for the package.
 # Each module also defines its own __all__.
-__all__ = ["__version__"]
+__all__ = ["__version__", "AdOpManager"]
