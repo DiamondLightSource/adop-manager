fa82d97be74ac1e160430cd3f22ee67cbf2084bc
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-07-17 08:27:15 +0100

    Added ConfigXmlParser to __init__.py

--- src/adop_manager/__init__.py
+++ src/adop_manager/__init__.py
@@ -1,6 +1,7 @@
 from ._version_git import __version__
 from .app import AdOpManager
+from .dataaccess import ConfigXmlParser
 
 # __all__ defines the public API for the package.
 # Each module also defines its own __all__.
-__all__ = ["__version__", "AdOpManager"]
+__all__ = ["__version__", "AdOpManager", "ConfigXmlParser"]
