fffa8398d3c702244f098c72312356b923946821
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-07-18 11:40:56 +0100

    Updated __main__.py from example

--- src/adop_manager/__main__.py
+++ src/adop_manager/__main__.py
@@ -1,20 +1,42 @@
+import asyncio
 from argparse import ArgumentParser
+from pathlib import Path
 
 from . import __version__
-from .hello import HelloClass, say_hello_lots
+from .app import AdOpManager
 
 __all__ = ["main"]
 
 
-def main(args=None):
+async def main(args=None):
     parser = ArgumentParser()
-    parser.add_argument("--version", action="version", version=__version__)
-    parser.add_argument("name", help="Name of the person to greet")
-    parser.add_argument("--times", type=int, default=5, help="Number of times to greet")
+
+    parser.add_argument("--version", "-v", action="version", version=__version__)
+
+    parser.add_argument("name", "--name", "-n", type=str, required=True)
+    parser.add_argument(
+        "script_call", "--script-call", "-s", type=tuple[str, ...], required=True
+    )
+    parser.add_argument("log_path", "--log-path", "-l", type=Path, required=True)
+    parser.add_argument("config_path", "--config-path", "-c", type=Path, required=True)
+    parser.add_argument("mirror1", "--mirror1", "-m1", type=str, required=True)
+    parser.add_argument("mirror2", "--mirror2", "-m2", type=str, required=True)
+    parser.add_argument(
+        "timeout", "--timeout", "-t", type=int, required=False, default=30
+    )
+
     args = parser.parse_args(args)
-    say_hello_lots(HelloClass(args.name), args.times)
+    AdOpManager(
+        args.name,
+        args.script_call,
+        args.log_path,
+        args.config_path,
+        args.mirror1,
+        args.mirror2,
+        args.timeout,
+    )
 
 
 # test with: pipenv run python -m adop_manager
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
